c4408d1c04fdf9fadb390a42d1bab41886462fa6
abhinavLaptop <krabhinav216@gmail.com>
2023-01-30 09:21:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/explicitintentapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.explicitintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.textView);
+        btn1 = findViewById(R.id.btn);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Explicit intent
+
+                Intent i = new Intent(getApplicationContext(), Activity2.class);
+
+                // send data between activities
+                i.putExtra("id","1000");
+
+                startActivity(i);
+            }
+        });
+    }
+}
